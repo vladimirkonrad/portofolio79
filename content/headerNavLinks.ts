2a78fd90dc021d14ed0e42156001148514d0484d
Vladimir Konrad <vladimirkonrad@gmail.com>
2024-11-26 09:00:51 +0100

    support link added

--- content/headerNavLinks.ts
+++ content/headerNavLinks.ts
@@ -1,6 +1,7 @@
 const headerNavLinks = [
   // { href: '/blog', title: 'Blog' },
   // { href: '/projects', title: 'Projects' },
+  { href: 'https://support.itworks.rs', title: 'Support' },
   { href: '/about', title: 'About' },
   { href: 'https://vladimirkonrad.com', title: 'Me' },
   // { href: '/uses', title: 'Uses' },
